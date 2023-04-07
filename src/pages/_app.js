e9398e8463f327a097cd2db33b4a5abf305d4701
ConnorHoare <connor@tipcigroup.com>
2023-04-07 20:15:45 +0100

    project setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+import '../styles/globals.css';
+import { Navbar, Footer } from '../components';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />;
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/7d15c82b1b.js" crossorigin="anonymous" />
+  </ThemeProvider>;
+};
+
+export default App;
